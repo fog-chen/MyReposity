530cce6e3ef4ea3fc5da2fa2aaf4dea529785054
chenorth@163.com <clan@umu360.com>
2018-12-11 00:31:41 +0800

    Revert "Revert "仿ios时钟""
    This reverts commit a1acd58ef5b7b83de084a7e26ce35a12c7698b9c.

---
+++ PosSystem/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <i class="icon iconfont icon-gongnengjianyi"></i> -->
+    <leftNav></leftNav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import leftNav from '@/components/common/leftNav'
+export default {
+  name: 'App',
+  components: { leftNav }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+</style>
